1f0fe4cad7d58fca8dbad7bdcc1ae05f07b53a82
Dat T Nguyen <datn@Dats-MacBook-Pro.local>
2021-08-29 10:30:32 +0700

    fix param fasttager

--- FastTagger/pipeline_source.sh
+++ FastTagger/pipeline_source.sh
@@ -1,4 +1,4 @@
-n#!/bin/bash
+#!/bin/bash
 
 # Author: Dat T Nguyen <n.dat@outlook.com>
 # Date: 15 Aug 2021
@@ -12,11 +12,9 @@ in_vcf=$1
 i=$2
 base_name=$3
 
+sample_param=/path/to/param_example.txt
+cat $sample_param | sed "s/max_tagSNP_num=30000/max_tagSNP_num=${i}/g" > ${i}_param.txt
 /path/to/preprocessing_FastTagger.R vcf=${in_vcf} out_prefix=${base_name}
-cat $sample_param | sed "s/max_tagSNP_num=30000/max_tagSNP_num=${i}/g" > ${i}_param.txt
-mkdir ./N_${i}
-sample_param=param_example.txt
-cat $sample_param | sed "s/max_tagSNP_num=30000/max_tagSNP_num=${i}/g" > ${i}_param.txt
 mkdir ./N_${i}
 /path/to/FastTaggerV2 ${base_name}.maf ${base_name}.matrix ./N_${i}/${base_name}_${i} ${i}_param.txt &
 /path/to/export_FastTager.R ${base_name}.maf ./N_${i}/${base_name}_${i}.tagSNP.txt
